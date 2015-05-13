156818b99b10c2f23e8b2d9753ccd5be3048693f
GermanBluefox <dogafox@gmail.com>
2015-05-13 22:04:51 +0200

    Update eiscp.js

--- eiscp.js
+++ eiscp.js
@@ -184,6 +184,7 @@ function command_to_iscp(command, args, zone) {
             return;
         }
     }
+    self.emit('debug', util.format('DEBUG (command_to_iscp) raw command "%s"', prefix + value));
 
     return prefix + value;
 }
